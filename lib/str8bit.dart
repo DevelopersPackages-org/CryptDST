d5cc30ef7e89b076fa7d092b0b0935b960e722ab
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-05-23 00:43:24 +0900

    add: baseprog

---
+++ lib/str8bit.dart
@@ -0,0 +1,13 @@
+void main() {
+  String testStr = "It's Test！";
+  expect(testStr,Str8bit.bin2str(Str8bit.str2bin(testStr)));
+}
+class Str8bit {
+  Uint8List str2bin(String str){
+
+  }
+  String bin2str(Uint8List uiol){
+
+  }
+}
+
